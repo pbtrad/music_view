34dc2d4ede72750c117e707b74666965f59f271b
paul <paulbrowne2@gmail.com>
2022-02-10 16:44:27 +0000

    Added dummy data

--- music_view/music_view/urls.py
+++ music_view/music_view/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('api/blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
